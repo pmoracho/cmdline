67e2e2d191a19bbe878ff302cea4836f5de12cd4
Patricio <pmoracho@gmail.com>
2022-05-28 20:30:29 -0300

    Nueva clase config

--- cmdline/config.py
+++ cmdline/config.py
@@ -1,31 +1,57 @@
 from configparser import ConfigParser
-import ast
+from pydoc import locate
 
 class Config:
 
-    DEF_CFG = {
-        "progress_bar_ticks": 5
-        }
+    DEF_CFG = {"lista_cadenas": ["jpg"],
+               "lista_enteros": [20, 20, 20, 20],
+               "float": 1.0,
+               "entero": 150,
+               "booleano": False,
+               "cadena": ''
+            }
 
-    def __init__(self, file=None, override=None):
+    def __init__(self, file=None, override_section=None):
 
-        self.file = file
         self.config = ConfigParser()
         self.__dict__.update(self.DEF_CFG)
-        if self.file:
-            self._load()
-
-    def set_file(self, file):
 
         self.file = file
+        self.override_section = override_section
+
         if self.file:
-            self._load()
+            if isinstance(self.file, str):
+              self.file = open(self.file, "rt")
 
-    def _load(self):
+            self._load('Config')
+            if self.override_section:
+                  self._load(self.override_section)
 
-        self.config.read(self.file)
+    def _load(self, section):
 
-        self.progress_bar_ticks = int(self.config.get("GLOBAL", "progress_bar_ticks"))
+        self.config.read_file(self.file)
+        items_ini = self.config.items(section)
 
-    def __str(self):
-        return str(self.__dict__)
+        for (k, v) in items_ini:
+
+          if k in self.__dict__:
+            d = self.__dict__[k]
+            if isinstance(d, list):
+                first = d[0]
+                self.__dict__[k] = list(map(locate(type(first).__name__),
+                                            [x.strip() for x in v.split(',')]))
+            else:
+                self.__dict__[k] = locate(type(d).__name__)(v)
+
+    def __str__(self):
+
+      return f"""
+-------------- Config -------------------
+lista_cadenas: {self.lista_cadenas}
+lista_enteros: {self.lista_enteros}
+        float: {self.float}
+       entero: {self.entero}
+     booleano: {self.booleano}
+       cadena: {self.cadena}
+-----------------------------------------
+      """
